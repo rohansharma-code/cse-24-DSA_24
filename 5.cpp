de0bfe2c466cf2da6619cbaebe0a0cf4817c6393
rohansharma-code <rohan.25b01011093@abes.ac.in>
2025-12-09 09:30:54 +0530

    string5

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    string str1 = "Hi! Welcome Home";
+    string str2 = "I am Nik";
+    cout << str1.insert(4, "Joy ") << endl;
+    cout << str1.insert(4, str2) << endl;
+    cout << str1.insert(21, ". What will you have?", 4,5) << endl;
+    return 0;
+}
